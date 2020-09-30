0b0b4e090dc84f41fd165b18ff5d85000c4725a8
Gedewon <gedewon.hayle.9@gmail.com>
2020-09-30 19:26:08 +0300

    spa injection

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,18 +3,37 @@ import Menu from "./MenuComponent";
 import Header from "./HeaderComponet";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import DISHDETAIL from "./DishdetailComponent";
 import Home from "./HomeCompont";
+import Contact from "./ContactComponent";
+
 import { Switch, Route, Redirect } from "react-router-dom";
 class Main extends Component {
   state = {
     dishes: DISHES,
     selectedDish: null,
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS,
   };
 
-  // onDishSelect(dishId) {
-  //   this.setState({ selectedDish: dishId });
-  // }
+  DishWithId = ({ match }) => {
+    return (
+      <DISHDETAIL
+        dish={
+          this.state.dishes.filter(
+            (dish) => dish.id === parseInt(match.params.dishId, 10)
+          )[0]
+        }
+        comments={this.state.comments.filter(
+          (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+        )}
+      />
+    );
+  };
   render() {
     return (
       <div>
@@ -32,12 +51,26 @@ class Main extends Component {
         /> */}
 
         <Switch>
-          <Route path="/home" component={() => <Home />} />
+          <Route
+            path="/home"
+            component={() => (
+              <Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={
+                  this.state.promotions.filter((promo) => promo.featured)[0]
+                }
+                leader={this.state.leaders.filter((lead) => lead.featured)[0]}
+              />
+            )}
+          />
           <Route
             exact
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route path="/menu/:dishId" component={this.DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+
           <Redirect to="/home" />
         </Switch>
         <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 const Menu = (props) => {
   //   note that when ever rendering a unique id in react we should give the key parameter's
   const menu = props.dishes.map((dish) => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
-        <Card key={dish.id} onClick={() => props.onClick(dish.id)}>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardImgOverlay>
-            <CardTitle>{dish.name}</CardTitle>
-          </CardImgOverlay>
+        <Card key={dish.id}>
+          <Link to={`/menu/${dish.id}`}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Link>
         </Card>
       </div>
     );
   });
   return (
     <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home </Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
+        </div>
+      </div>
       <div className="row">{menu}</div>
     </div>
   );
